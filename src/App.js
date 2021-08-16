b9788039c2f7565af74283ad27387767d67c17ef
Michaela Guzmická <mgmichaelaguzmicka@gmail.com>
2021-08-16 13:25:18 +0200

    add and import PlayList

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './styles/App.scss';
 import Player from './components/Player';
 import Song from './components/Song';
+import data from './components/PlayList';
 
 const App = () => {
   return (

2e9935c40930f32439d7b2bac047b76cd3fea065
Michaela Guzmická <mgmichaelaguzmicka@gmail.com>
2021-08-17 14:31:38 +0200

    create play/pause, create time update, update slider input

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,9 +1,10 @@
-import React, { useRef } from 'react';
+import React, { useState, useRef } from 'react';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlay, faAngleLeft, faAngleDoubleRight, faAngleRight } from '@fortawesome/free-solid-svg-icons';
+import { faPlay, faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
 
 const Player = ({ isPlaying, setIsPlaying, currentSong }) => {
+
 	const audioRef = useRef(null);
 	const playHandler = () => {
 		if (isPlaying) {
@@ -14,21 +15,55 @@ const Player = ({ isPlaying, setIsPlaying, currentSong }) => {
 			setIsPlaying(!isPlaying);
 		}
 	};
+
+	const timeUpdateHandler = (e) => {
+		const current = e.target.currentTime;
+		const duration = e.target.duration;
+		setSongSpan({ ...songSpan, currentTime: current, duration });
+	};
+
+	const getTime = (time) => {
+		return (
+			Math.floor(time / 60) + ':' + ('0' + Math.floor(time % 60)).slice(-2)
+		)
+	};
+
+	const dragHandler = (e) => {
+		audioRef.current.currentTime = e.target.value;
+		setSongSpan({...songSpan, currentTime: e.target.value});
+	};
+
+	const [songSpan, setSongSpan] = useState({
+		currentTime: 0,
+		duration: 0,
+	});
+
 	return (
 		<div className='player'>
 			<div className='time-control'>
-				<p>Start Time</p>
-				<input type='range' />
-				<p>End Time</p>
+				<p>{getTime(songSpan.currentTime)}</p>
+				<input
+					min={0}
+					max={songSpan.duration}
+					value={songSpan.currentTime}
+					onChange={dragHandler}
+					type='range'
+				/>
+				<p>{getTime(songSpan.duration)}</p>
 			</div>
 			<div className='play-control'>
 				<FontAwesomeIcon className='play-back' size='2x' icon={faAngleLeft} />
 				<FontAwesomeIcon onClick={playHandler} className='play' size='2x' icon={faPlay} />
 				<FontAwesomeIcon className='play-forward' size='2x' icon={faAngleRight} />
 			</div>
-			<audio ref={audioRef} src={currentSong.audio} />
+			<audio
+				onTimeUpdate={timeUpdateHandler}
+				onLoadedMetadata={timeUpdateHandler}
+				ref={audioRef}
+				src={currentSong.audio}
+			/>
 		</div>
-	)
-}
+	);
+};
 
 export default Player;
